0ae5294d06f63c6e702127112c7b7c0fbfbafe97
ahsi-at <aisha_ditta@hotmail.com>
2024-10-25 12:22:24 +0100

    color values
    updating h2 element to display colour values

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function colorButton(){
     const randomColor = `#` + Math.floor(Math.random()*77215).toString(16);
     document.body.style.backgroundColor = randomColor;
     console.log(randomColor);
+    colourvalue.textContent = randomColor;
+
 
 }
 
@@ -28,6 +30,7 @@ function pastelcolorButton(){
     const pastelrandomColor = `rgb(${r}, ${g}, ${b})`;
     document.body.style.backgroundColor = pastelrandomColor;
     console.log(pastelrandomColor);
+    colourvalue.textContent = pastelrandomColor;
    
 
 }
@@ -38,13 +41,15 @@ pastelbtn.addEventListener("click", pastelcolorButton);
 
 // ! display colour value on screen
 
-const colourvalue = document.getElementById("colourvalue");
+// const colourvalue = document.getElementById("colourvalue");
 
-function displayValue(){
-    const value = randomColor *  `#` +(Math.floor(Math.random()*77215).toString(16));
-    colourvalue.textContent = value; 
+// function displayValue(){
+//     const value = randomColor *  `#` +(Math.floor(Math.random()*77215).toString(16));
+//     colourvalue.textContent = value; 
 
-}
-displayValue();
+// }
+// displayValue();
 
-Generatebtn.addEventListener("click", displayValue);
+// Generatebtn.addEventListener("click", displayValue);
+
+/* this didnt work because randomcolor is a variable in another function and cant be accessed.*/
