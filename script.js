f57c2eb0971874f7f1ed6db15b3ad7c6a7371f9d
ahsi-at <aisha_ditta@hotmail.com>
2024-10-25 11:24:36 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const Generatebtn = document.getElementById("Generatebtn");
+
+function colorButton(){
+    const randomColor = `#` + Math.floor(Math.random()*77215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+    console.log(randomColor);
+
+}
+
+
+colorButton()
+
+Generatebtn.addEventListener("click", colorButton);
+
+
+
+
+//  ! generate a random pastel colour
+
+
+const pastelbtn = document.getElementById("pastelbtn"); /* access button*/
+
+function pastelcolorButton(){
+    const r = Math.floor((Math.random() * 127) + 127);
+    // const r = Math.floor((Math.random() * 17) + 127);
+    const g = Math.floor((Math.random() * 127) + 127);
+    const b = Math.floor((Math.random() * 127) + 127);
+    const pastelrandomColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = pastelrandomColor;
+    console.log(pastelrandomColor);
+   
+
+}
+pastelcolorButton()
+
+pastelbtn.addEventListener("click", pastelcolorButton);
+
+
